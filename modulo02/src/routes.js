de232d83c1b1ecf6739a5b91ef1d87d6f417c277
rodrigoferraro <ferraro.rodrigo@gmail.com>
2020-03-29 00:38:57 -0300

    config nodemon-sucrase Debug

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  return res.json({ message: "Oilá" });
+  return res.json({ message: "Oilá Mondo" });
 });
 
 export default routes;
