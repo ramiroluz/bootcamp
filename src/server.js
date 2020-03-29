4fd9b132065ffd6d11d09df88baf8fa9ddbf3198
Ramiro Batista da Luz <ramiroluz@gmail.com>
2020-03-29 15:12:02 -0300

    Migration de usuário.

---
+++ src/server.js
@@ -0,0 +1,13 @@
+// import app from './app';
+//
+// app.listen(3333);
+
+console.log('TODO app!!!!!!!!!!!!!!!!');
+
+console.log('TODO app!!!!!!!!!!!!!!!!');
+console.log('TODO app!!!!!!!!!!!!!!!!');
+console.log('TODO app!!!!!!!!!!!!!!!!');
+console.log('TODO app!!!!!!!!!!!!!!!!');
+
+console.log('TODO app!!!!!!!!!!!!!!!!');
+console.log('TODO app!!!!!!!!!!!!!!!!');
