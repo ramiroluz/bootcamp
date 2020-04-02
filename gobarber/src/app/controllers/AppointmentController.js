24e18144e71755555c8949beacb6624162064901
Deini Porto <dasporto@yahoo.com.br>
2020-04-02 11:52:17 -0300

    Incluir agendamento de serviço

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -1,7 +1,38 @@
-class AppointmetController {
+import * as Yup from 'yup';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
     async store(req, res) {
-        return res.json({ ok: true });
+        const schema = Yup.object().shape({
+            provider_id: Yup.number().required(),
+            date: Yup.date().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return req.status(400).json({ error: 'Erro de validação' });
+        }
+
+        const { provider_id, date } = req.body;
+
+        const isProvider = await User.findOne({
+            where: { id: provider_id, provider: true },
+        });
+
+        if (!isProvider) {
+            return req
+                .status(404)
+                .json({ error: 'Só pode criar agendamentos com provedores' });
+        }
+
+        const appointment = await Appointment.create({
+            user_id: req.userId,
+            provider_id,
+            date,
+        });
+
+        return res.json(appointment);
     }
 }
 
-export default new AppointmetController();
+export default new AppointmentController();
